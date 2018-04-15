8cbc3d4926d19fda1d8cf9a95865cb6d5b0aa3ca
SurovRuslan <ruslansurov@MacBook-Air-Ruslan.local>
2018-04-15 18:01:07 +0300

    add files

--- script.js
+++ script.js
@@ -1,38 +1,106 @@
-let students = [
-    {
-        name: "Petya",
-        startStud: 2015,
-        finishStud: 2020,
-    },
-    {
-        name: "Vasya",
-        startStud: 2012,
-        finishStud: 2027,
-    },
-    {
-        name: "Kolya",
-        startStud: 2008,
-        finishStud: 2013,
-    },
-    {
-        name: "Ira",
-        startStud: 2022,
-        finishStud: 2027,
-    },
-    {
-        name: "Sveta",
-        startStud: 2015,
-        finishStud: 2016,
-    },
-];
-function showList(startEd, finishEd) {
-    for (let i = 0; i < students.length; i++){
-        if (startEd <= students[i].startStud && students[i].startStud <= finishEd
-            ||
-            finishEd >= students[i].finishStud && students[i].finishStud <= finishEd){
-            console.log(students[i].name);
-        }
-    }
-};
-showList(2010, 2015);
+// let students = [
+//     {
+//         name: "Petya",
+//         startStud: 2015,
+//         finishStud: 2020,
+//     },
+//     {
+//         name: "Vasya",
+//         startStud: 2012,
+//         finishStud: 2027,
+//     },
+//     {
+//         name: "Kolya",
+//         startStud: 2008,
+//         finishStud: 2013,
+//     },
+//     {
+//         name: "Ira",
+//         startStud: 2022,
+//         finishStud: 2027,
+//     },
+//     {
+//         name: "Sveta",
+//         startStud: 2015,
+//         finishStud: 2016,
+//     },
+// ];
+// function showList(startEd, finishEd) {
+//     for (let i = 0; i < students.length; i++){
+//         if (startEd <= students[i].startStud && students[i].startStud <= finishEd
+//             ||
+//             finishEd >= students[i].finishStud && students[i].finishStud <= finishEd){
+//             console.log(students[i].name);
+//         }
+//     }
+// };
+// showList(2010, 2015);
+//
 
+
+
+
+//
+// var companyName = prompt ('companyName?', '');
+// if (companyName == ('qwerty')) {
+//     alert ('right!');
+// }
+// else {
+//     alert ('wrong!');
+// };
+
+// var numb = prompt ('enter number', '');
+// if (numb > 0) {
+//     alert (1);
+// }
+// else if (numb < 0) {
+//     alert (-1);
+// }
+// else {
+//     alert (0);
+// };
+
+// var userName = prompt ('Name?', '');
+// if (userName == (null)) {
+//     alert ("canceled");
+// }
+// else if (userName = ("admin")) {
+//     var password;
+//     prompt('password', '');
+//         if (password = ('molly')) {
+//             alert ('welcome');
+//         }
+//         else if (password == null) {
+//             alert ('canceled');
+//         }
+//         else {
+//             alert ('wrong');
+//         }
+// }
+// else {
+//     alert ('i dont know you');
+// };
+
+let number = {
+    a: 4,
+    b: 3,
+    result: 0,
+    sum: function () {
+        this.sumRes = this.a + this.b;
+        return this
+    },
+    minus: function () {
+        this.minRes = this.a - this.b;
+        return this
+    },
+    multyplay: function () {
+        this.multRes = this.a * this.b;
+        return this
+    },
+    showResalt: function () {
+        console.log (this.sumRes, this.minRes, this.multRes);
+
+    },
+};
+
+number.sum().minus().multyplay().showResalt();
