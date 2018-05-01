1d015eb325ac5a3a148c288def51d2a87a7c8236
SurovRuslan <ruslansurov@MacBook-Air-Ruslan.local>
2018-05-01 16:09:19 +0300

    add dateTable

--- layOut/script.js
+++ layOut/script.js
@@ -7,3 +7,27 @@ menu.onclick = function myFunction() {
         x.className = "topnav";
     }
 };
+
+
+var fullDate = new Date ();
+var day = fullDate.toLocaleString('en-US', {weekday: "long"});
+var month = fullDate.toLocaleString('en-US', {month: "long"});
+var date = fullDate.getDate();
+var year = fullDate.getFullYear();
+var hour = fullDate.getHours();
+var minute = fullDate.getMinutes();
+var second = fullDate.getSeconds();
+var millisecond = fullDate.getMilliseconds();
+var today = `Date: ${day} ${month} ${date} ${year}
+ Time: ${hour} : ${minute} : ${second}`;
+
+
+
+var buttonDate = document.createElement("button");
+buttonDate.id = "myButton";
+var dateTable = document.getElementById("dateTable");
+dateTable.appendChild(buttonDate);
+buttonDate.innerHTML = today;
+
+
+
